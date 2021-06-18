3d941ab510e8cdb203620f6d37d80272255a1a81
jk <mudgolem333@gmail.com>
2021-06-18 11:19:33 +0800

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" 
+            src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v11.0" nonce="BgLqn8GT">
+        </script>
+        <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" 
+            data-layout="default" data-auto-logout-link="false" data-use-continue-as="false">
+        </div>
+    </body>
+</html>
