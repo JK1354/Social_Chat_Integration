d00c30ccad1dea0b207b18a776164d8bcb787b2f
jk <mudgolem333@gmail.com>
2021-06-21 17:11:24 +0800

    login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,37 @@
 <!DOCTYPE html>
 <html>
     <head>
-    
+        <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+            appId      : '819787645577614',
+            cookie     : true,
+            xfbml      : true,
+            version    : '1.0.0'
+            });
+            
+            FB.AppEvents.logPageView();   
+            
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        // FB.getLoginStatus(function(response) {
+        //     statusChangeCallback(response);
+        // });
+
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+        }
+        </script>
     </head>
     <body>
         <div id="fb-root"></div>
@@ -11,5 +41,12 @@
         <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" 
             data-layout="default" data-auto-logout-link="false" data-use-continue-as="false">
         </div>
+
+        <fb:login-button 
+            scope="public_profile,email"
+            onlogin="checkLoginState();">
+        </fb:login-button>
+
+
     </body>
 </html>
